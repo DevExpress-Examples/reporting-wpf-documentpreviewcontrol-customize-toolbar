8ef7d7eacf3b67a33783fc5874f6a9b60a84cc38
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomizePreviewToolbar/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows;
+using DevExpress.Xpf.Printing;
+// ...
+
+namespace CustomizePreviewToolbar {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private SimpleLink link;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            // Create a document to display.
+            string[] data = CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
+
+            link = new SimpleLink {
+                DetailTemplate = (DataTemplate)Resources["dayNameTemplate"],
+                DetailCount = data.Length
+            };
+            link.CreateDetail += (s, e) => e.Data = data[e.DetailIndex];
+
+            preview.DocumentSource = link;
+            link.CreateDocument();
+        }
+
+        private void CreateDocument_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            link.CreateDocument();
+        }
+
+        private void ClearPreview_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            link.PrintingSystem.ClearContent();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# DocumentPreviewControl - How to remove standard bar items and add custom ones to the toolbar
+
+
+<p>This example demonstrates how to customize the <strong>DocumentViewerControl's </strong>toolbar (either <a href="https://documentation.devexpress.com/#WPF/CustomDocument6554">BarManager</a> or <a href="https://documentation.devexpress.com/#WPF/CustomDocument7954">RibbonControl</a>).</p>
+<p>In particular, it shows how to remove standard commands from a Ribbon toolbar and add custom ones, as well as customize command icons.</p>
+
+<br/>
+
+
