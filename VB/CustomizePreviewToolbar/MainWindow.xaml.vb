be76eb861f06d6b161e0f0d3a67d07e6ce194d44
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomizePreviewToolbar/MainWindow.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Globalization
+Imports System.Windows
+Imports DevExpress.Xpf.Printing
+' ...
+
+Namespace CustomizePreviewToolbar
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Private link As SimpleLink
+
+        Public Sub New()
+            InitializeComponent()
+
+            ' Create a document to display.
+            Dim data() As String = CultureInfo.CurrentCulture.DateTimeFormat.DayNames
+
+            link = New SimpleLink With {.DetailTemplate = DirectCast(Resources("dayNameTemplate"), DataTemplate), .DetailCount = data.Length}
+            AddHandler link.CreateDetail, Sub(s, e) e.Data = data(e.DetailIndex)
+
+            preview.DocumentSource = link
+            link.CreateDocument()
+        End Sub
+
+        Private Sub CreateDocument_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            link.CreateDocument()
+        End Sub
+
+        Private Sub ClearPreview_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            link.PrintingSystem.ClearContent()
+        End Sub
+    End Class
+End Namespace
